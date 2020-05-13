6ea7f1bc38a4d2afa63510602dc9e212ebb1b5af
Andran1 <Andran1@users.noreply.github.com>
2020-05-13 16:32:43 +0400

    Updates

---
+++ precache-manifest.5707b134a8bd9c25b76add0e48191e9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026f3686e45ede12348200a1e173cab9",
+    "url": "/15puzzle-react/index.html"
+  },
+  {
+    "revision": "ea8623cd95d6ac43719b",
+    "url": "/15puzzle-react/static/css/main.28e07883.chunk.css"
+  },
+  {
+    "revision": "e316501caa59414316ad",
+    "url": "/15puzzle-react/static/js/2.38ed950b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/15puzzle-react/static/js/2.38ed950b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea8623cd95d6ac43719b",
+    "url": "/15puzzle-react/static/js/main.90067a4b.chunk.js"
+  },
+  {
+    "revision": "9b87cb5a2bf18fb127c0",
+    "url": "/15puzzle-react/static/js/runtime-main.1ae745ed.js"
+  }
+]);
